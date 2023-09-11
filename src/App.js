ea0c8bda38d38e6f44d6c07728238597c46f617a
nmthong226 <nmthong226@gmail.com>
2023-09-11 13:48:38 +0700

    react-hook-day3-printlist

--- src/App.js
+++ src/App.js
@@ -5,14 +5,28 @@ import { useState } from 'react'
 
 function App() {
   let [name, setName] = useState('Minh Thong');
+
   const [address, setAddress] = useState('');
+
+  const [ToDos, setToDos] = useState([
+    {ID: 'todo1', title: 'Watching Youtube'},
+    {ID: 'todo2', title: 'Coding'},
+    {ID: 'todo3', title: 'Playing Game'}
+  ]);
+
   const HandleClick = (event) => {
-    setName(address);
-    console.log('>>>> Click me', address);
+    if (!address)
+    {
+      alert('no empty input!'); 
+      return;
+    }
+    let newTodo = {id: 'abc', title: address};
+    setToDos([...ToDos, newTodo]);
+    setAddress('');
   }
+
   const HandleOnChange = (event) => {
     setAddress(event.target.value);
-    console.log(event.target.value);
   }
   //re-render
   return (
@@ -21,6 +35,13 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>{name}'s creating Website</h1>
+        <div className = "Task Container">
+          {ToDos.map(ToDos => {
+            return (
+              <li className='Task Child' key = {ToDos.ID}> {ToDos.title} </li>
+            )
+          })}
+        </div>
         <input type = "text" value = {address} onChange={(event) => HandleOnChange(event)}/>
         <button type = "button" onClick={(event) => HandleClick(event)}>Click me</button> 
       </header>
