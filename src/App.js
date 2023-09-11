92746998844f70706a7327361f285a6ee8dd53c9
nmthong226 <nmthong226@gmail.com>
2023-09-11 12:49:44 +0700

    react-hook-day3

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
 import Nav from './views/Nav.js';
+import { useState } from 'react'
 
 function App() {
+  let [name, setName] = useState('Minh Thong');
+  const [address, setAddress] = useState('');
   const HandleClick = (event) => {
-    console.log('>>>> Click me', event.target.value);
+    setName(address);
+    console.log('>>>> Click me', address);
   }
-
+  const HandleOnChange = (event) => {
+    setAddress(event.target.value);
+    console.log(event.target.value);
+  }
+  //re-render
   return (
     <div className="App">
       <Nav />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>NguyenMinhThong creating Website</h1>
-        <input type = "text" value = "MinhThong" onClick={(event) => HandleClick(event)}/>
-        <button type = "button" onClick={(event) => HandleClick(event)}>Click me</button>
+        <h1>{name}'s creating Website</h1>
+        <input type = "text" value = {address} onChange={(event) => HandleOnChange(event)}/>
+        <button type = "button" onClick={(event) => HandleClick(event)}>Click me</button> 
       </header>
     </div>
   );
