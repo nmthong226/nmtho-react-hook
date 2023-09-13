1dd0bd4957579ed2abf92571be5c5eee09178919
nmthong226 <nmthong226@gmail.com>
2023-09-13 15:25:53 +0700

    react-day5-weatherDemo

--- src/views/Covid.js
+++ src/views/Covid.js
@@ -5,37 +5,50 @@ import axios from 'axios';
 const Covid = () => {
 
   const [DataTmp, setDataTmp] = useState([]);
-    async function fetchData() {
-        let response = await axios('https://api.openweathermap.org/data/2.5/weather?id=1566083&appid=ad0e1f9d4eed22b12cd1ced1ac97a2d5');
-        let data = response && response.data ? response.data: [];
-        console.log(response.data);
-        setDataTmp(data);
-      }
+  const [Loading, setLoading] = useState(true);
+  async function fetchData() {
+    let response = await axios('https://api.openweathermap.org/data/2.5/weather?id=1566083&appid=ad0e1f9d4eed22b12cd1ced1ac97a2d5');
+    let data = response && response.data ? response.data: [];
+    console.log(response.data);
+    setDataTmp(data);
+    setLoading(false);
+  }
 
-    useEffect(() => {
-        fetchData(); 
-    }, []);
-    
+  useEffect(() => {
+    setTimeout(async () => {
+      fetchData();
+    }, 2000);
+  }, []);
+
     return(
         <table>
           <thead>
             <tr>
               <td>City</td>
-              <td>Temperature</td>
+              <td>Temperature (K)</td>
               <td>Humid</td>
-              <td>Weather</td>
+              <td>Wind speed</td>
             </tr>
           </thead>
           <tbody>
-            <tr key ={DataTmp.id}>
+            {(Loading === false) &&
+              <tr key ={DataTmp.id}>
                   <td>{DataTmp.name}</td>
-                  <td>{DataTmp.main.temp_max - 273}</td>
-                  <td>{DataTmp.main.humidity}</td>
-                  <td>{DataTmp.weather[0].description}</td>
-                </tr>
+                  <td>{DataTmp?.main?.temp_max}</td>
+                  <td>{DataTmp?.main?.humidity}</td>
+                  <td>{DataTmp?.wind?.speed}</td>
+              </tr>
+            }
+            {
+              (Loading === true) &&
+              <tr>
+                <td  colspan = '5' style ={{textAlign: 'center'}}>
+                  Loading...
+                </td>
+              </tr>
+            }
           </tbody>
         </table>
     )
 }
-
 export default Covid;
